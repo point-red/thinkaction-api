f36efddd0e063e575ccf936ce3d0c106e62a98d7
Ali Akbar <ali5alkaf5@gmail.com>
2024-08-26 02:21:31 +0700

    App Export

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ dotenv.config();
 
 import { router } from './routes';
 import Database from './database/database';
+const PORT: Number = Number(process.env.PORT) || 5050;
 
 const init = async () => {
   await Database.init();
@@ -27,6 +28,7 @@ const init = async () => {
   app.use('/', (req: Request, res: Response, next: NextFunction): void => {
     res.json({});
   });
+  app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
   return app;
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import App from "./app";
 import Database from "./database/database";
 
-const PORT: Number = Number(process.env.PORT) || 5050;
-
-App.init().then((app) => {
-  app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
-})
+App.init()
 
 process.on('SIGINT', () => Database.disconnect('SIGINT'));
 process.on('SIGTERM', () => Database.disconnect('SIGTERM'));
+
+module.exports = App;
