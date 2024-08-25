9e7632e65f916bb1d50326c6a67dd744398c9176
Ali Akbar <ali5alkaf5@gmail.com>
2024-08-26 04:16:08 +0700

    Server Update

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from "express";
 import App from "./app";
 import Database from "./database/database";
 
@@ -6,7 +7,10 @@ process.on('SIGTERM', () => Database.disconnect('SIGTERM'));
 
 if (process.env.VERCEL === '1') {
   // If in Vercel, export the app for serverless function handling
-  module.exports = () => App.init();
+  module.exports = async (req: Request, res: Response) => {
+    const app = await App.init();
+    app(req, res);
+  };
 } else {
   // If running locally, start the server normally
   App.init();
