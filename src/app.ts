eaef5529e5dc9d0d131972774032dbf20c37ae3c
Martien Dermawan Tanama <martiendt@gmail.com>
2024-11-03 15:51:33 +0700

    fix: remove credentials and unused code

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,40 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { json, urlencoded } from 'body-parser';
-import cookieParser from 'cookie-parser';
-import { errorMiddleware } from './middleware/error.middleware';
+import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middleware/error.middleware";
 
 dotenv.config();
 
-import { router } from './routes';
-import Database from './database/database';
+import { router } from "./routes";
 const PORT: Number = Number(process.env.PORT) || 5050;
 
 const init = async () => {
   const app: Application = express();
 
-  // if (process.env.NODE_ENV !== 'production') {
-  // if (!global.mongodbClient) {
-  // await Database.init();
-  // }
-  // }
-
-  app.use(cors({
-    credentials: true,
-    origin: process.env.APP_URL
-  }));
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.APP_URL,
+    })
+  );
   app.use(cookieParser());
   app.use(json());
   app.use(urlencoded({ extended: true }));
 
-  app.use('/v1', router);
+  app.use("/v1", router);
   app.use(errorMiddleware);
 
-  app.use('/', (req: Request, res: Response, next: NextFunction): void => {
+  app.use("/", (req: Request, res: Response, next: NextFunction): void => {
     res.json({});
   });
-  if (process.env.VERCEL !== '1') {
+  if (process.env.VERCEL !== "1") {
     app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
   }
   return app;
-}
+};
 
 export default {
-  init
+  init,
 };
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,11 +1,11 @@
-import { Collection, Db, MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
+import { Collection, Db, MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default class Database {
-  private uri: string = process.env.MONGODB_URI || 'mongodb+srv://zildanmarginata:9i0GEZR8vImJTcuI@mymongodb.htazbwd.mongodb.net/mymongodb?retryWrites=true&w=majority';
-  private dbName: string = 'think_action';
+  private uri: string = process.env.MONGODB_URI as string;
+  private dbName: string = process.env.DATABASE_NAME as string;
   public db!: Db;
   private client: MongoClient;
   public collection!: Collection;
@@ -16,9 +16,9 @@ export default class Database {
       this.client.connect();
       this.db = this.client.db(this.dbName);
       this.collection = this.db.collection(collection);
-      console.log('Berhasil terhubung ke MongoDB');
+      console.log("Berhasil terhubung ke MongoDB");
     } catch (error) {
-      console.error('Gagal terhubung ke MongoDB:', error);
+      console.error("Gagal terhubung ke MongoDB:", error);
       throw error;
     }
   }
